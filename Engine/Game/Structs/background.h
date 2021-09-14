30e0a0fc31366c6ab4295ccf40a17f0147928c1b
Tyler C <18288340+TylerCode@users.noreply.github.com>
2021-09-14 11:29:16 -0600

    ayyyy

---
+++ Engine/Game/Structs/background.h
@@ -0,0 +1,8 @@
+typedef struct Background
+{
+    float posX;
+    float posY;
+    float width;
+    float height;
+    char filename[256];
+} Background;
---
+++ Engine/Game/game.c
@@ -0,0 +1,27 @@
+#include "Structs/background.h";
+#include "Structs/character.h";
+#include "dialogbox.h";
+#include <time.h>
+#include <stdbool.h>
+
+//I will try to keep all the directly SDL related shit in here.
+//This file will be a chonker bonker but then if you want to port
+//the engine to say, the PSP (something I want to do) then you only
+//have to change this one file.
+
+void DrawScreen(void);
+
+int game(void)
+{
+    bool quit = false;
+}
+
+void DrawScreen(void)
+{
+    // Draw the background
+    // Draw the character(s)
+    // Draw the decorations (like sun shafts, rain, etc)
+    // Draw the text box
+    // Draw the buttons
+    // Draw the text
+}
