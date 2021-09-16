b995bbb9d082ad20bd6b462286ad6babd22f5a5c
Tyler C <18288340+TylerCode@users.noreply.github.com>
2021-09-16 16:30:54 -0600

    Trying my best it seems

--- Engine/Game/game.c
+++ Engine/Game/game.c
@@ -1,12 +1,11 @@
-#include "Structs/background.h";
-#include "Structs/character.h";
-#include "Structs/state.h";
-#include "Structs/dialogbox.h";
-#include <time.h>;
-#include <stdbool.h>;
-#include <SDL2/SDL.h>;
-#include <SDL2/SDL_image.h>;
-#include <iostream>;
+#include "Structs/background.h"
+#include "Structs/character.h"
+#include "Structs/state.h"
+#include "Structs/dialogbox.h"
+#include <time.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 //I will try to keep all the directly SDL related shit in here.
 //This file will be a chonker bonker but then if you want to port
@@ -16,6 +15,7 @@
 void DrawMainScreen(void);
 void DrawMenu(void);
 bool Initialize(void);
+void Shutdown(void);
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
@@ -25,6 +25,7 @@ int game(void)
 {
     bool quit = false;
     gameState.state = 0;
+    atexit(Shutdown);
 
     if(!Initialize())
     {
@@ -48,15 +49,17 @@ int game(void)
 
 bool Initialize(void)
 {
+    SDL_Init(SDL_INIT_VIDEO);
+
     //Gets graphics shit ready
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        fprintf(stderr,
-            "What the cinnamon toast fuck is this? No compatible graphics?!!\nHere is your stupid error: ",
-            SDL_GetError());
-        printf(SDL_GetError());
-        return false;
-    }
+    // if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    // {
+    //     fprintf(stderr,
+    //         "What the cinnamon toast fuck is this? No compatible graphics?!!\nHere is your stupid error: ",
+    //         SDL_GetError());
+    //     printf(SDL_GetError());
+    //     return false;
+    // }
 
     window = SDL_CreateWindow("Game",
         SDL_WINDOWPOS_CENTERED,
@@ -67,6 +70,10 @@ bool Initialize(void)
 
     if(!window)
     {
+        fprintf(stderr, "Could not create SDL window: %s\n", SDL_GetError() );
+        SDL_Delay(3000);
+        SDL_Quit();
+        //std::cout << "Something got effed in the window!\n";
         return false;
     }
 
@@ -74,6 +81,7 @@ bool Initialize(void)
 
     if(!renderer)
     {
+        //std::cout << "Something got effed in the renderer!\n";
         return false;
     }
 
@@ -92,9 +100,17 @@ void DrawMainScreen(void)
 
 void DrawMenu(void)
 {
+    SDL_SetRenderDrawColor(renderer, 0,0,0,255);
+    SDL_RenderClear(renderer);
+
     //std::cout << "Draw Menu\n";
     // Draw the background
     // Draw the text box
     // Draw the buttons
     // Draw the text
+}
+
+void Shutdown(void)
+{
+    //std::cout << "Shutting down...\n";
 }
--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Visual Novel engine written completely in C
 # Build command (idfk)
 `g++ -std=c++17 main.c -I"include" -L"lib" -Wall -lSDL2main -lSDL2 -lSDL2_image -o test`
 
+or 
+
+`gcc main.c -I/usr/local/include -L/usr/local/lib -lSDL2main -lSDL2 -lSDL2_image -o test`
+
 All example material sourced from: https://lemmasoft.renai.us/forums/viewtopic.php?t=17302
